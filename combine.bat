b738bef07981455f0fb5d55e1abf0b3a8d85ff67
jacobvc <jacobvc@users.noreply.github.com>
2023-05-17 11:39:38 -0500

    Some file renaming

--- combine.bat
+++ combine.bat
@@ -1,4 +1,4 @@
-python combine.py -o standalone/csv-xml-view.html index.html options.js table-create.js xml-convert.js style.css 
+python combine.py -o standalone/XmlCsvReader.html xmlcsv.html options.js table-create.js xml-convert.js style.css 
 python combine.py -o standalone/ChurchTracBudgetReport.html churchtrac.html options.js table-create.js style.css 
 
 pause
